47145e7639052fbec585b13e1cea9dc474a199f7
Pau <32746505+paufraba@users.noreply.github.com>
2017-10-15 11:12:02 +0200

    Añadir id a los heroes.

--- 02.SPA/src/app/components/search/search.component.ts
+++ 02.SPA/src/app/components/search/search.component.ts
@@ -9,6 +9,7 @@ import { HeroesService, Heroe } from '..\\..\\services\\heroes.service';
 export class SearchComponent implements OnInit {
 
     heroes:Heroe[] = [];
+    texto:string;
 
     constructor(private activatedRoute: ActivatedRoute,
         private _heroesService:HeroesService) {
@@ -17,6 +18,7 @@ export class SearchComponent implements OnInit {
         ngOnInit() {
             this.activatedRoute.params.subscribe(params=>{
                 // console.log(params['text'])
+                this.texto = params['text'];
                 this.heroes = this._heroesService.buscarHeroes(params['text']);
                 console.log(this.heroes);
             })
--- 02.SPA/src/app/services/heroes.service.ts
+++ 02.SPA/src/app/services/heroes.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 export class HeroesService {
     private heroes:Heroe[] = [
         {
+            id: "1",
             nombre: "Aquaman",
             bio: "El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.",
             img: "assets/img/aquaman.png",
@@ -11,6 +12,7 @@ export class HeroesService {
             casa:"DC"
         },
         {
+            id: "2",
             nombre: "Batman",
             bio: "Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.",
             img: "assets/img/batman.png",
@@ -18,6 +20,7 @@ export class HeroesService {
             casa:"DC"
         },
         {
+            id: "3",
             nombre: "Daredevil",
             bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
             img: "assets/img/daredevil.png",
@@ -25,6 +28,7 @@ export class HeroesService {
             casa: "Marvel"
         },
         {
+            id: "4",
             nombre: "Hulk",
             bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
             img: "assets/img/hulk.png",
@@ -32,6 +36,7 @@ export class HeroesService {
             casa:"Marvel"
         },
         {
+            id: "5",
             nombre: "Linterna Verde",
             bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)",
             img: "assets/img/linterna-verde.png",
@@ -39,6 +44,7 @@ export class HeroesService {
             casa: "DC"
         },
         {
+            id: "6",
             nombre: "Spider-Man",
             bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
             img: "assets/img/spiderman.png",
@@ -46,6 +52,7 @@ export class HeroesService {
             casa: "Marvel"
         },
         {
+            id: "7",
             nombre: "Wolverine",
             bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
             img: "assets/img/wolverine.png",
@@ -63,7 +70,11 @@ export class HeroesService {
     }
 
     getHeroe(idx:string):Heroe {
-        return this.heroes[idx];
+        for (let heroe of this.heroes) {
+            if (heroe.id == idx)
+                return heroe;
+        }
+        // return this.heroes[idx];
     }
 
     buscarHeroes(texto: string):Heroe[]{
@@ -83,6 +94,7 @@ export class HeroesService {
 }
 
 export interface Heroe {
+    id:string;
     nombre:string;
     bio:string;
     img:string;
