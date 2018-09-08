dea0054531e794fea1c8f0dea277dd3287ce93c7
Pau <32746505+paufraba@users.noreply.github.com>
2018-09-08 17:25:49 +0200

    Sección 18, vídeo 271

--- 15.FotosFirebase/src/app/components/fotos/fotos.component.ts
+++ 15.FotosFirebase/src/app/components/fotos/fotos.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { nombre: string; url: string; }
 
 @Component({
-  selector: 'app-fotos',
-  templateUrl: './fotos.component.html',
-  styles: []
+    selector: 'app-fotos',
+    templateUrl: './fotos.component.html',
+    styles: []
 })
 export class FotosComponent implements OnInit {
+    private itemsCollection: AngularFirestoreCollection<Item>;
+    items: Observable<Item[]>;
 
-  constructor() { }
+    constructor(private afs: AngularFirestore) {
+        this.itemsCollection = afs.collection<Item>('img');
+        this.items = this.itemsCollection.valueChanges();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
